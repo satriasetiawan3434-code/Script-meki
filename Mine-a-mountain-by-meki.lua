253c40114ca4e1f1a22ccffef7fe024e7320ac7e
MekinjirT5 <satriasetiawan3434@gmail.com>
2026-08-25 11:54:49 +0700

    Update Mine-a-mountain-by-meki.lua

--- Mine-a-mountain-by-meki.lua
+++ Mine-a-mountain-by-meki.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/satriasetiawan3434-code/Script-meki/refs/heads/main/Mine-a-mountain.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/satriasetiawan3434-code/Script-meki/refs/heads/main/Maklow.lua"))()
