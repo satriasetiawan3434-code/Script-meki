8083d52647d139e5394bc2302da7767ed5491bf2
MekinjirT5 <satriasetiawan3434@gmail.com>
2026-08-31 18:49:25 +0700

    Update Meki-hub-pistol-arena.lua

--- Meki-hub-pistol-arena.lua
+++ Meki-hub-pistol-arena.lua
@@ -62,7 +62,7 @@ getKeyBtn.TextSize = 14
 Instance.new("UICorner", getKeyBtn).CornerRadius = UDim.new(0, 6)
 
 getKeyBtn.MouseButton1Click:Connect(function()
-    local url = "https://chat.whatsapp.com/E7ZorHopiwPLn5HfUFfLNi?s=cl&p=a&mlu=4"
+    local url = "https://direct-link.net/3821198/c0U2lgPgMnJA"
     -- Coba salin ke clipboard
     local success = pcall(function()
         setclipboard(url)
